2f7c3bc02e8779ddbd3c96b5477f7c78e44d88c6
Diego Rubin <rubin.diego@gmail.com>
2014-09-01 13:24:14 -0300

    Prefix user session attributes.

--- lib/luna/helpers.rb
+++ lib/luna/helpers.rb
@@ -3,21 +3,23 @@ module Luna
 
     def authenticate_luna_user!
       if !luna_user_signed_in?
-        redirect_to open_session_path && return
+        redirect_to open_session_path
+        return
       end
     end
 
     def current_luna_user
       if luna_user_signed_in?
         @user ||= User.new({
-          email: session['user_email'], token: session['user_token']
+          email: session['luna_user_email'], token: session['luna_user_token'],
+          id: sessions['luna_user_id']
         })
       end
     end
 
     def luna_user_signed_in?
-      !session['user_id'].blank? && !session['user_email'].blank? &&
-        !session['user_token'].blank?
+      !session['luna_user_id'].blank? && !session['luna_user_email'].blank? &&
+        !session['luna_user_token'].blank?
     end
 
   end
