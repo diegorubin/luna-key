430a281a49c6c27f5f07de3373d7750100a46f4e
Diego Rubin <rubin.diego@gmail.com>
2014-09-01 13:11:37 -0300

    New methods in helper.

--- lib/luna/helpers.rb
+++ lib/luna/helpers.rb
@@ -1,12 +1,25 @@
 module Luna
   module Helpers
+
+    def authenticate_luna_user!
+      if !luna_user_signed_in?
+        reditect_to :open_session_path && return false
+      end
+    end
+
     def current_luna_user
-      if !session['user_email'].blank? && !session['user_token'].blank?
+      if luna_user_signed_in?
         @user ||= User.new({
           email: session['user_email'], token: session['user_token']
         })
       end
     end
+
+    def luna_user_signed_in?
+      !session['user_id'].blank? && !session['user_email'].blank? &&
+        !session['user_token'].blank?
+    end
+
   end
 end
 
