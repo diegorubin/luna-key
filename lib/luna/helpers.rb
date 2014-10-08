d8af7cc79c74344108036753037cc2bf4341057f
Diego Rubin <rubin.diego@gmail.com>
2014-10-08 19:42:02 -0300

    Fix method name.

--- lib/luna/helpers.rb
+++ lib/luna/helpers.rb
@@ -13,9 +13,9 @@ module Luna
     end
 
     def current_luna_user
-      email = cookie['luna_user_email'] || session['luna_user_email']
-      token = cookie['luna_user_token'] || session['luna_user_token']
-      id = cookie['luna_user_id'] || session['luna_user_id']
+      email = cookies['luna_user_email'] || session['luna_user_email']
+      token = cookies['luna_user_token'] || session['luna_user_token']
+      id = cookies['luna_user_id'] || session['luna_user_id']
 
       if luna_user_signed_in?
         @current_luna_user ||= User.new({
@@ -27,14 +27,14 @@ module Luna
     def luna_user_signed_in?
       !session['luna_user_id'].blank? && !session['luna_user_email'].blank? &&
         !session['luna_user_token'].blank? ||
-        !cookie['luna_user_id'].blank? && !cookie['luna_user_email'].blank? &&
-        !cookie['luna_user_token'].blank?
+        !cookies['luna_user_id'].blank? && !cookies['luna_user_email'].blank? &&
+        !cookies['luna_user_token'].blank?
     end
 
     def luna_user_sign_in(user, remember = false)
       ['id', 'email', 'token'].each do |attr|
         if remember
-          cookie["luna_user_#{attr}"] = user.send(attr)
+          cookies["luna_user_#{attr}"] = user.send(attr)
         else
           session["luna_user_#{attr}"] = user.send(attr)
         end
@@ -44,7 +44,7 @@ module Luna
     def luna_user_sign_out
       ['id', 'email', 'token'].each do |attr|
         session["luna_user_#{attr}"] = nil
-        cookie["luna_user_#{attr}"] = nil
+        cookies["luna_user_#{attr}"] = nil
       end
     end
 
